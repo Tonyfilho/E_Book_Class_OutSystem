407af1eeeaea598b4a17c1f9b13bcf264b06ca0c
tonyfilho <oficinadosdrones@gmail.com>
2023-10-15 14:19:56 +0100

    fixing directories

--- Debora_OutSystem/src/app/_start-components/start-components.module.ts
+++ Debora_OutSystem/src/app/_start-components/start-components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './body/header/header.component';
+import { FooterComponent } from './body/footer/footer.component';
 import { BodyComponent } from './body/body.component';
 import { RouterModule } from '@angular/router';
 
