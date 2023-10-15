1b278e61be038d9830076570c3e614a8cc382ff3
tonyfilho <oficinadosdrones@gmail.com>
2023-10-15 09:29:51 +0100

    The project already start

--- Debora_OutSystem/src/app/start-components/start-components.module.ts
+++ Debora_OutSystem/src/app/start-components/start-components.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -13,7 +14,11 @@ import { BodyComponent } from './body/body.component';
     BodyComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    BodyComponent
   ]
 })
 export class StartComponentsModule { }
