1b46ceb3280b378a0d4f4e390bdf7c1acd03057e
tonyfilho <oficinadosdrones@gmail.com>
2023-10-31 18:07:05 +0000

    mat-tool bar

--- Debora_OutSystem/src/app/app.module.ts
+++ Debora_OutSystem/src/app/app.module.ts
@@ -34,6 +34,7 @@ import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AuthenticationModule } from './_start-pages/authentication/authentication.module';
 import { StartComponentsModule } from './_start-pages/start-components/start-components.module';
+import { _ShareMaterialModule } from './_start-pages/_share/_share-material.module';
 
 
 @NgModule({
@@ -68,7 +69,7 @@ import { StartComponentsModule } from './_start-pages/start-components/start-com
     ReactiveFormsModule,
     AuthenticationModule,
     StartComponentsModule,
-
+    _ShareMaterialModule,
     OutsystemModule,
     AngularFireAuthModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
--- Debora_OutSystem/src/app/outsystem/outsystem.module.ts
+++ Debora_OutSystem/src/app/outsystem/outsystem.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OutsystemComponent } from './outsystem.component';
+import { _ShareMaterialModule } from '../_start-pages/_share/_share-material.module';
 
 
 
@@ -9,7 +10,8 @@ import { OutsystemComponent } from './outsystem.component';
     OutsystemComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    _ShareMaterialModule
   ]
 })
 export class OutsystemModule { }
